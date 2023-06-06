429d5353bc9e42ad8038afb01df47a886f42e606
daveamarante <xt202001849@wmsu.edu.ph>
2023-06-06 22:30:18 +0800

    .

--- js/script.js
+++ js/script.js
@@ -87,7 +87,20 @@
         prevArrow: $('.prev'),
         nextArrow: $('.next'),
     }); 
-
+    var slidePosition = 0;
+    SlideShow();
+    
+    function SlideShow() {
+      var i;
+      var slides = document.getElementsByClassName("Containers");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slidePosition++;
+      if (slidePosition > slides.length) {slidePosition = 1}
+      slides[slidePosition-1].style.display = "block";
+      setTimeout(SlideShow, 2000); // Change image every 2 seconds
+    } 
     $('.product-grid').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
